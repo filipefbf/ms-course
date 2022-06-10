b4fe08a047da414a3481e240c2f6bb33a06d60d2
Filipe Barbosa Ferreira <filipefbf@ibm.com>
2022-06-10 18:25:03 -0300

    UserFeignClient

--- hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/User.java
@@ -1,30 +1,21 @@
-package com.devsuperior.hruser.entities;
+package com.devsuperior.hroauth.entities;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@Entity
-@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(unique = true)
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
+
     private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
 
@@ -66,8 +57,6 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public Set<Role> getRoles() { return roles;}
 
     @Override
     public boolean equals(Object o) {
